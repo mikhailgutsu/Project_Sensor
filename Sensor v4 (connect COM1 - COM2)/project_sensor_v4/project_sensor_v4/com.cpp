b2a2f746f22f999bf4f22903fd62800553775383
mikhailgutsu <mikhail.gutsu.2002@gmail.com>
2023-03-06 13:27:11 +0200

    com.cpp file updated

--- Sensor v4 (connect COM1 - COM2)/project_sensor_v4/project_sensor_v4/com.cpp
+++ Sensor v4 (connect COM1 - COM2)/project_sensor_v4/project_sensor_v4/com.cpp
@@ -1,47 +1,64 @@
-﻿#include "com.h"
+﻿//------- USER Includes
+#include "com.h"
 
+/*********************************************************
+             Communication from COM1 -> Com2
+*********************************************************/
 int COM::com1()
 {
+    //------------ Open L"COM PORT 1"
     LPCTSTR sPortName = L"COM1";
 
+    //------------ Create hSerial_File
     hSerial = ::CreateFile(sPortName, GENERIC_READ | GENERIC_WRITE, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
 
-    //////////////////////////////////////////////////////
+    //------------ CheckLoop
     if (hSerial == INVALID_HANDLE_VALUE)
     {
-        if (GetLastError() == ERROR_FILE_NOT_FOUND)
+        if (GetLastError() == ERROR_FILE_NOT_FOUND)                       //< IF Serial Port is close
         {
             std::cout << "serial port does not exist.\n";
         }
         std::cout << "some other error occurred.\n";
     }
-    ///////////////////////////////////////////////////////
+    //------------ End ChackLoop
+
+    //------------ Clear Params and = with 0
     DCB dcbSerialParams = { 0 };
+
+    //------------ Lenght = sizeof params
     dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
 
+    //------------ If params is empty 
     if (!GetCommState(hSerial, &dcbSerialParams))
     {
         std::cout << "getting state error\n";
     }
 
+    //------------ Setings
     dcbSerialParams.BaudRate = CBR_9600;
     dcbSerialParams.ByteSize = 8;
     dcbSerialParams.StopBits = ONESTOPBIT;
     dcbSerialParams.Parity = NOPARITY;
 
+    //------------ If params is empty 
     if (!SetCommState(hSerial, &dcbSerialParams))
     {
         std::cout << "error setting serial port state\n";
     }
 
-    char data[] = "Hello from C++";  // 
-    DWORD dwSize = sizeof(data);   // 
-    DWORD dwBytesWritten;    // 
+    //------------ INIT
+    char data[] = "Hello from C++";  
+    DWORD dwSize = sizeof(data);   
+    DWORD dwBytesWritten;     
 
+    //------------ Write file
     BOOL iRet = WriteFile(hSerial, data, dwSize, &dwBytesWritten, NULL);
 
+    //------------ COUT
     std::cout << dwSize << " Bytes in string. " << dwBytesWritten << " Bytes sended. " << std::endl;
 
+    //------------ Loop
     while (1)
     {
         ReadCOM();
@@ -49,19 +66,25 @@ int COM::com1()
     return 0;
 }
 
+/****************************************************************
+                Read data from COM PORT
+****************************************************************/
 void COM::ReadCOM(void)
 {
+    //------------- Open
     DWORD iSize;
 
     while (true)
     {
-        ReadFile(hSerial, &sReceivedChar, 1, &iSize, 0);  // ïîëó÷àåì 1 áàéò
+        //-------- Reading file from char_chr
+        ReadFile(hSerial, &sReceivedChar, 1, &iSize, 0); 
 
-        if (iSize > 0)   // åñëè ÷òî-òî ïðèíÿòî, âûâîäèì
+        if (iSize > 0)   //< If isn't empty
             std::cout << sReceivedChar;
     }
 }
 
+//----------- START
 int COM::scan()
 {
     return com1();
