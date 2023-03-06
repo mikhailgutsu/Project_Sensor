135cfab70aad712d267a877b0e42f684b96cb931
mikhailgutsu <mikhail.gutsu.2002@gmail.com>
2023-03-06 12:17:15 +0200

    print.h update

--- Sensor v4 (connect COM1 - COM2)/project_sensor_v4/project_sensor_v4/print.h
+++ Sensor v4 (connect COM1 - COM2)/project_sensor_v4/project_sensor_v4/print.h
@@ -1,28 +1,36 @@
+//------- HEADER INCLUDE
 #pragma once
 #include <iostream>
 #include <windows.h>
 #include <ctime>
 #include <chrono>
 
-#define Lines 20
-#define Columns 120
+//------- USER DEFINES
+#define Lines 20											//<		Lines OY axe
+#define Columns 120											//<		Columns OX axe
 #define loop() for(;;)
 
+//------- Print class
 class Print
 {
 private:
-	std::string Graph[Lines][Columns];						//<
-	int counter = 1;										//<
-	int temp;												//<
-	int forCounter_i = 0;									//<
-	int forCounter_j = 0;									//<
+	std::string Graph[Lines][Columns];						//<		Predefinition of our graph[20][120]
 
-	void setGraph();										//< 
-	void srandGraph(int m_temp, int counter);				//<
-	void printGraph();										//<
-	void moveGraph();										//<
-	void process();											//<
+	int counter = 1;										//<		Firts Pos of counter (for '*')
+	int temp;												//<		Temp variable 
+
+	//---------- Temp vars for alghorithm
+	int forCounter_i = 0;									
+	int forCounter_j = 0;									
+
+	//---------- Predef of methods
+	void setGraph();										//<		Set graph
+	void srandGraph(int m_temp, int counter);				//<		Set '*'
+	void printGraph();										//<		Print
+	void moveGraph();										//<		Move graph after <counter> pos
+	void process();											//<		Start methods
 
 public:
-	void sensor(int m_temp);								//<
+	//---------- Start Menu
+	void sensor(int m_temp);								
 };
