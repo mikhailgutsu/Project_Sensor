3c2be610f74b20b8bd8a63b8661a1e2a96acca73
mikhailgutsu <mikhail.gutsu.2002@gmail.com>
2023-03-06 11:55:31 +0200

    main update

--- Sensor v4 (connect COM1 - COM2)/project_sensor_v4/project_sensor_v4/project_sensor_v4.cpp
+++ Sensor v4 (connect COM1 - COM2)/project_sensor_v4/project_sensor_v4/project_sensor_v4.cpp
@@ -1,17 +1,20 @@
-﻿#include "print.h"
-#include "com.h"
+﻿//--------------------------
+#include "print.h"										//<		Include print.h file
+#include "com.h"										//<		Include com.h file
 
+//--------------------------
 int main()
 {
-	Print obj;
-	COM comScan;
-	int m_temp;
+	Print obj;											//<		Create obj from Print Class
+	COM comScan;										//<		Create obj from COM Class
+	int m_temp;											//<		Temporary variable for COM data
 
-	loop()
+	loop()												//<		Defined Loop for(;;)
 	{
-		m_temp = comScan.scan();
-		obj.sensor(m_temp);
+		m_temp = comScan.scan();						//<		Insert data from COM Port to temp var
+		obj.sensor(m_temp);								//<		Print on XY axe
 	}
 
 	return 0;
 }
+//--------------------------
